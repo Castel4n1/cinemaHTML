086f6833559ea2927b9cab4f8fcd6817ec0de5de
KaueSenac <kaue1castelani@gmail.com>
2022-06-09 20:02:01 -0300

    Primeiras configurações.

---
+++ base.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vísio Play</title>
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- /CSS -->
+</head>
+<body>
+<!-- CONTAINER -->
+    <div class="container">
+    <!-- CABEÇALHO -->
+    <!-- CABEÇALHO -->
+    <!-- CONTEÚDO -->
+    <div class="row">
+        <h1>conteudo</h1>
+    </div>
+    <!-- CONTEÚDO -->
+    <!-- RODAPÉ -->
+    <!-- RODAPÉ -->
+    </div>
+<!-- /CONTAINER -->
+</body>
+<!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/js/script.js"></script>
+<!-- /JS -->
+
+</html>
